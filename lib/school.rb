0c24d8102657aebf569e7bb4c9479aa922933fca
Justin Tooley <jstooley823@gmail.com>
2017-09-10 06:11:31 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,13 +9,13 @@ class School
 
   def add_student(name, grade)
     if @roster.key?(:grade)
-      @roster[:grade] << name
+      @roster[grade] << name
     else
-      @roster[:grade] = []
-      @roster[:grade] << name
+      @roster[grade] = []
+      @roster[grade] << name
     end
     def grade(grade)
-      @roster[:grade]
+      @roster[grade]
     end
     def sort
       @roster.collect do |grade, names|
