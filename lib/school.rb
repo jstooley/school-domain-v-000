fef6b6c80f4ce03d66f224a4367344cbe1e2285c
Justin Tooley <jstooley823@gmail.com>
2017-09-10 05:30:51 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,9 +1,14 @@
 # code here!
 class School
-  attr_reader :name
+  attr_reader :name :roster
+
+  ROSTER = {}
   
   def initialize(name)
     @name = name
+  end
+
+  def roster=(name)
   end
 end
 school = School.new("Hayside High School")
